033949b1d779b8e6059faf3b9a5fac27a38f19df
MAKIA YENGUE GODWILL <godwillmakiayengue@gmail.com>
2026-04-30 19:06:38 +0100

    modified:   src/lib/axios.ts

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,5 +3,8 @@ import axios from "axios";
 export const axiosInstance = axios.create({
   baseURL: "https://mgy-spotify-clone-backend-ixu0xbp0c-makirow-mys-projects.vercel.app/api",
   withCredentials: true,        // ← THIS IS VERY IMPORTANT!
- 
+  headers: {
+    "Content-Type": "application/json",
+     'x-vercel-protection-bypass': "hsdlb2CJnISZT9gGfgp2L7wU0q6HkVPx"
+  },
 });
