952bc688bb29c719c8379de9b1d5ce5ffc2d3b43
MAKIA YENGUE GODWILL <godwillmakiayengue@gmail.com>
2026-04-30 01:51:18 +0100

    On branch main Your branch is up to date with 'origin/main'.

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import { ClerkProvider } from "@clerk/clerk-react";
+
 import { BrowserRouter } from "react-router-dom";
-import AuthProvider from "./providers/AuthProvider.tsx";
+
 
 const PUBLISHABLE_KEY = "pk_test_aGlwLW1pbmstNTkuY2xlcmsuYWNjb3VudHMuZGV2JA";
 
