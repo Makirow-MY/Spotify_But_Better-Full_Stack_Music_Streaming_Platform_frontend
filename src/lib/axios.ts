4253383d5e1b5255c04b1b6e1fff406326ef849c
MAKIA YENGUE GODWILL <godwillmakiayengue@gmail.com>
2026-05-01 17:50:48 +0100

    Your branch is up to date with 'origin/main'.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,10 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://spotify-clone-backend-pi.vercel.app/api",
+  baseURL: "https://spotify-clone-backend-rose.vercel.app/api",
   withCredentials: true,        // ← THIS IS VERY IMPORTANT!
   headers: {
     "Content-Type": "application/json",
-     'x-vercel-protection-bypass': "sC9QaNcXLtEn5uEJ57CwE9L869AOPjwC"
-  },
+     },
 });
